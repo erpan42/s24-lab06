733ff539ff7e384d081bda1c5cbe6a29d9d06a4d
erpan42 <yierpan42@gmail.com>
2024-02-20 21:49:57 -0500

    task 2

--- Java/src/main/java/frogger/Frogger.java
+++ Java/src/main/java/frogger/Frogger.java
@@ -13,19 +13,13 @@ public class Frogger {
     
     // Field for task 2. Anything to add/change?
     private final Records records;
-    private String firstName, lastName, phoneNumber, zipCode, state, gender;
+    private FroggerID froggerID;
 
-    public Frogger(Road road, int position, Records records, String firstName, String lastName, String phoneNumber,
-    String zipCode, String state, String gender) {
+    public Frogger(Road road, int position, Records records, String firstName, String lastName, String phoneNumber, String zipCode, String state, String gender) {
         this.road = road;
         this.position = position;
         this.records = records;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.zipCode = zipCode;
-        this.state = state;
-        this.gender = gender;
+        this.froggerID = new FroggerID(firstName, lastName, phoneNumber, zipCode, state, gender);
     }
 
     /**
@@ -44,6 +38,9 @@ public class Frogger {
     }
 
     // TODO: Do you notice any issues here? methods moved to Road.java for encapsulation
+    // Feature envy: some of the functionality in the Frogger class might more appropriately belong to the Road class
+    // The Frogger class is overly reliant on the internal details of the Road class when it had methods
+    // here with implementation details.
 
 
     /**
@@ -52,7 +49,7 @@ public class Frogger {
      * @return true if record successful, else false.
      */
     public boolean recordMyself() {
-      boolean success = records.addRecord(firstName, lastName, phoneNumber, zipCode, state, gender);
+      boolean success = records.addRecord(froggerID);
       return success;
     }
 
