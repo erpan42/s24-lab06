9eb04cf54debc2a5ee4c2da583ab7aa0688ac807
erpan42 <yierpan42@gmail.com>
2024-02-20 22:02:05 -0500

    task 3

--- Java/src/main/java/drawing/Drawing.java
+++ Java/src/main/java/drawing/Drawing.java
@@ -30,10 +30,19 @@ public class Drawing {
      */
     public void draw(String format, String filename) {
         // TODO: Do you notice any issues here?
+        // Use strategy pattern to refactor the code
+        // Creating a WriterStrategy interface with a method like write(Shape shape).
+        // Implementing this interface for each supported file format.
+        // Modifying the Drawing class to accept a WriterStrategy instance and use it in the draw method
+        // removing the format-specific code branches.
+
         if (format.equals("jpeg")) {
             try (Writer writer = new JPEGWriter(filename + ".jpeg")) {
                 for (Shape shape : this.shapes) {
                     // TODO: What is the issue of the behavior here?
+                    // encapsulation problem again, let each Shape handle its conversion to lines internally. 
+                    // The Shape interface (or abstract class) should have a method that directly accepts a writer and draws itself.
+                    // The Drawing class's draw method would simply iterate over the shapes and call this new method, without needing to manage lines
                     Line[] lines = shape.toLines();
                     shape.draw(writer, lines);
                 }
